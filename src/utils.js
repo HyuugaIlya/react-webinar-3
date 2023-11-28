22d93d9e9400655056f5a5911824b0da99e9cb1a
HyuugaIlya <HyuugaIlya@users.noreply.github.com>
2023-11-29 00:17:41 +0300

    Revert "Update utils.js"
    This reverts commit f859a295f522013c700de2c1eda3f51b8cb6ad3f.

--- src/utils.js
+++ src/utils.js
@@ -37,8 +37,8 @@ export function setCountTimes(count) {
   const strEnd = str.at(str.length - 1);
   const strLast = str.at(str.length - 2) + strEnd;
 
-  if (strEnd >= '2' && strEnd <= '4') {
-    if (strLast >= '12' && strLast <= '14') {
+  if (strEnd === '2' || strEnd === '3' || strEnd === '4') {
+    if (strLast === '12' || strLast === '13' || strLast === '14') {
       return 'раз'
     } else {
       return 'раза'
